704b4b09cff62583f401cf97b836aa2f02093496
ChrisLam <fdq09eca@gmail.com>
2021-03-26 06:11:09 +0000

    fmt

--- Arkanoid/Arkanoid/Ball.hpp
+++ Arkanoid/Arkanoid/Ball.hpp
@@ -40,7 +40,7 @@ struct Ball {
     }
     
     bool collision(Brick& p, float delta_time) {
-        if (p.hp <= 0) return false;
+        if (p.hp <= 0)  return false;
 
         float b_nx = pos.x + velocity.x * delta_time;
         float b_ny = pos.y + velocity.y * delta_time;
@@ -56,132 +56,70 @@ struct Ball {
         Line p_right        (p_top_right, p_btm_right);
         Line p_btm          (p_btm_left, p_btm_right);
 
-
         Line ball_ray       (pos, ball_next_pos);
         Point intersection;
 
         if (ball_ray.intersection(p_top, intersection) || ball_ray.intersection(p_btm, intersection)) {
-            velocity.y = -velocity.y;
-            pos = intersection;
             p.hp--;
+            velocity.y  = -velocity.y;
+            pos         = intersection;
             return true;
         }
 
         if (ball_ray.intersection(p_left, intersection) || ball_ray.intersection(p_right, intersection)) {
-            velocity.x = -velocity.x;
-            pos = intersection;
             p.hp--;
+            velocity.x  = -velocity.x;
+            pos         = intersection;
             return true;
         }
+
         return false;
-        
-        
-        
-//        if (b.hp <= 0) return false;
-//
-//        float bx0 = b.pos.x;
-//        float bx1 = b.pos.x + b.width;
-//        float by0 = b.pos.y;
-//        float by1 = b.pos.y + b.height;
-//
-//        if (!(pos.x >= bx0 && pos.x <= bx1 && pos.y >= by0 && pos.y <= by1)) return false;
-//
-//        b.hp--;
-//
-//        float dy0 = fabs(pos.y - by0);
-//        float dy1 = fabs(pos.y - by1);
-//        float dx0 = fabs(pos.x - bx0);
-//        float dx1 = fabs(pos.x - bx1);
-//
-//        if (velocity.x < 0 && velocity.y < 0) {
-//            if (dy1 < dx1) {
-//                pos.y = by1;
-//                velocity.y = -velocity.y;
-//            } else {
-//                pos.x = bx1;
-//                velocity.x = -velocity.x;
-//            }
-//            return true;
-//        }
-//
-//        if (velocity.x < 0 && velocity.y > 0) {
-//            if (dy0 < dx1) {
-//                pos.y = by0;
-//                velocity.y = -velocity.y;
-//            } else {
-//                pos.x = bx1;
-//                velocity.x = -velocity.x;
-//            }
-//            return true;
-//        }
-//
-//        if (velocity.x > 0 && velocity.y > 0) {
-//            if (dy0 < dx0) {
-//                pos.y = by0;
-//                velocity.y = -velocity.y;
-//            } else {
-//                pos.x = bx0;
-//                velocity.x = -velocity.x;
-//            }
-//            return true;
-//        }
-//
-//        if (velocity.x > 0 && velocity.y < 0) {
-//            if (dy1 < dx0) {
-//                pos.y = by1;
-//                velocity.y = -velocity.y;
-//            } else {
-//                pos.x = bx0;
-//                velocity.x = -velocity.x;
-//            }
-//            return true;
-//        }
-//        return false;
     }
     
     void collision(Paddle& p, float delta_time) {
         float b_nx = pos.x + velocity.x * delta_time;
         float b_ny = pos.y + velocity.y * delta_time;
-        Point ball_next_pos(b_nx, b_ny);
         
-        Point p_top_left(p.pos.x, p.pos.y);
-        Point p_top_right(p.pos.x + p.width, p.pos.y);
-        Point p_btm_left(p.pos.x, p.pos.y + p.height);
-        Point p_btm_right(p.pos.x + p.width, p.pos.y + p.height);
+        Point ball_next_pos (b_nx, b_ny);
         
-        Line p_top(p_top_left, p_top_right);
-        Line p_left(p_top_left, p_btm_left);
-        Line p_right(p_top_right, p_btm_right);
+        Point p_top_left    (p.pos.x            , p.pos.y           );
+        Point p_top_right   (p.pos.x + p.width  , p.pos.y           );
+        Point p_btm_left    (p.pos.x            , p.pos.y + p.height);
+        Point p_btm_right   (p.pos.x + p.width  , p.pos.y + p.height);
+        
+        Line p_top      (p_top_left,    p_top_right );
+        Line p_left     (p_top_left,    p_btm_left  );
+        Line p_right    (p_top_right,   p_btm_right );
 //        Line p_btm(p_btm_left, p_btm_right);
         
         Line ball_ray(pos, ball_next_pos);
         Point intersection;
         
-        bool ball_hits_paddle = ball_ray.intersection(p_top, intersection)
-                             || ball_ray.intersection(p_left, intersection)
-                             || ball_ray.intersection(p_right, intersection);
+        bool ball_hits_paddle = ball_ray.intersection(p_top,    intersection)
+                             || ball_ray.intersection(p_left,   intersection)
+                             || ball_ray.intersection(p_right,  intersection);
         
         if (ball_hits_paddle) {
-            pos = intersection;
-            velocity.y = -velocity.y;
+            pos         = intersection;
+            velocity.y  = -velocity.y;
         }
     }
     
     void update(float delta_time) {
         
         if (pos.y < 0 ) {
-            velocity.y = -velocity.y;
-            pos.y = 0;
+            velocity.y  = -velocity.y;
+            pos.y       = 0;
         }
         
         if (pos.x < 0) {
-            velocity.x = -velocity.x;
-            pos.x = 0;
+            velocity.x  = -velocity.x;
+            pos.x       = 0;
         }
         
         if (pos.x >= SCREEN_W) {
-            velocity.x = -velocity.x;
-            pos.x = SCREEN_W;
+            velocity.x  = -velocity.x;
+            pos.x       = SCREEN_W;
         }
         
         
